52182afe167ed873979e4774fd245a6f97e16c5c
ngoloalidao-rgb <ngoloalidao@gmail.com>
2026-08-26 15:16:55 +0000

    Create MainActivity.kt
    package com.example.gerantprod
    import android.os.Bundle
    import androidx.activity.ComponentActivity
    import androidx.activity.compose.setContent
    // ... Le code complet de gestion de stock compose est disponible ...
    // dans le fichier ZIP du projet ou dans notre discussion.

---
+++ MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gerantprod
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+// ... Le code complet de gestion de stock compose est disponible ...
+// dans le fichier ZIP du projet ou dans notre discussion.
